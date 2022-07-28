2118965ab99e65874ca0eef28d3bcea8b4302bc5
Michael Machado <machado.m3@icloud.com>
2022-07-27 21:11:44 -0300

    first commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Card from "./Card"
+
+function ListItem(props) {
+
+    function DoneImg(props) {
+        if (props.done) {
+            return (<img alt="done" src="./assets/done.png"></img>)
+        } else {
+            return (<img alt="undone" src="./assets/notdone.png"></img>)
+        }
+    }
+
+
+    return (
+        <li >
+            <Card className={props.item.done ? 'done item' : 'item'}>
+                {props.item.text}
+                <div>
+                    <button onClick={() => { props.onDone(props.item) }}><DoneImg done={props.item.done}></DoneImg></button>
+                    <button onClick={() => { props.onItemDeleted(props.item) }}><img alt="delete" src="./assets/trash.png"></img></button>
+                </div>
+            </Card>
+        </li>)
+}
+
+export default ListItem
